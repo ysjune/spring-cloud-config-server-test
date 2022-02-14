c47bd941f9b26e58ffaf77bbdd80c84094cda587
Glen <sj.yeom@fitpet.co.kr>
2022-02-14 10:25:59 +0900

    Initial commit

---
+++ src/main/java/com/example/springcloudconfigservertest/SpringCloudConfigServerTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudconfigservertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigServerTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringCloudConfigServerTestApplication.class, args);
+  }
+
+}
